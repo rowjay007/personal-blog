423f9654e4a382948bd279a454d2201f2c4b1bf1
rowjay007 <rowlives@gmail.com>
2022-06-16 12:29:56 +0100

    installed and adding posts with frontmatter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 import Layout from "../components/Layout";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({posts}) => {
+  console.log(posts)
   return (
-    <Layout >
+    <Layout>
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <Head>
-          <title >Rowland Adimoha</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         <div>
           <h2>Hello, World</h2>
         </div>
@@ -19,5 +18,31 @@ const Home: NextPage = () => {
     </Layout>
   );
 };
+
+export async function getStaticProps(props: any) {
+  const files = fs.readdirSync(path.join("posts"));
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  console.log(posts);
+
+  return {
+    props: {
+      posts
+    },
+  };
+}
 
 export default Home;
