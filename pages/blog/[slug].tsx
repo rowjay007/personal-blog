32f98472aeeb94e2140a5a0076529e2752376c0a
rowjay007 <rowlives@gmail.com>
2022-06-30 00:46:02 +0100

    temp display of marked down text

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -2,13 +2,44 @@ import path from "path";
 import fs from "fs";
 import React from "react";
 import matter from "gray-matter";
+import * as marked from "marked";
+import Layout from "../../components/Layout";
+import Link from "next/link";
 
 const PostPage = ({
-  frontmatter: { title, category, data, cover_image, author, author_image },
+  frontmatter: { title, category, date, cover_image, author, author_image },
   content,
   slug,
 }) => {
-  return <div>PostPage</div>;
+  const sanitizedData = () => ({
+    __html: content,
+  });
+  return (
+    <Layout title={title}>
+      <Link href="/blog">Go Back</Link>
+      <div className="w-full px-10 py-10 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
+          <div className="">{category}</div>
+        </div>
+        <img src={cover_image} alt="" className="w-full rounded q" />
+        <div className="flex justify-between items items-center bg-gray-100 p-2 my-8">
+          <div>
+            <img
+              src={author_image}
+              alt=""
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            />
+            <h4>{author}</h4>
+          </div>
+          <div className="mr-4">{date}</div>
+        </div>
+        <div className="blog-text mt-2">
+          <div dangerouslySetInnerHTML={sanitizedData()} />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default PostPage;
